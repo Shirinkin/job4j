0ec4ba3d7f30a8016021f71aa2f06e1888fd29c2
Shirinkin <misha.shirinkin@gmail.com>
2018-11-15 22:06:57 +0300

    6.6. Двухмерный массив. Таблица умножения.[#86124]

---
+++ chapter_001/src/main/java/ru/job4j/array/Matrix.java
@@ -0,0 +1,26 @@
+package ru.job4j.array;
+
+/**
+ * Таблица умножения
+ * @author Misha shirinkin (Misha.shirinkin@gmail.com)
+ * @version $Id$
+ * @since 0.1
+ */
+public class Matrix {
+
+    /**
+     * Метод генерирует таблицу умножения
+     * @param size - размер таблицы
+     * @return таблица
+     */
+    public int[][] multiple(int size) {
+        int[][] table = new int[size][size];
+        for(int i = 0; i < size; i++) {
+            for (int k = 0; k < size; k++) {
+                table[i][k] = (i + 1) * (k + 1);
+            }
+        }
+        return table;
+    }
+
+}
---
+++ chapter_001/src/test/java/ru/job4j/array/MatrixTest.java
@@ -0,0 +1,40 @@
+package ru.job4j.array;
+
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * Тестирует класс матрицы
+ */
+public class MatrixTest {
+
+    /**
+     * Тестируем метод multiple с входным парааметром 2 на 2
+     */
+    @Test
+    public void when2on2() {
+        Matrix matrix = new Matrix();
+        int[][] table = matrix.multiple(2);
+        int[][] expect = {
+                {1, 2},
+                {2, 4}
+        };
+        assertThat(table, is(expect));
+    }
+
+    /**
+     * Тестируем метод multiple с входным парааметром 3
+     */
+    @Test
+    public void when3on3() {
+        Matrix matrix = new Matrix();
+        int[][] table = matrix.multiple(3);
+        int[][] expect = {
+                {1, 2, 3},
+                {2, 4, 6},
+                {3, 6, 9}
+        };
+        assertThat(table, is(expect));
+    }
+}
